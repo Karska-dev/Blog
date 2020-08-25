a9c29d5c4c81f65f21802387562597872ba8d035
Oleksandra <karska.dev@gmail.com>
2020-08-24 20:15:47 -0700

    Create about, article and articles-list pages. Add Route. Add css style

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ArticlePage from './pages/ArticlePage';
+import ArticlesList from './pages/ArticlesList';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
-    </div>
+    <Router>
+      <div className="App">
+        <div id="page-body">
+          <Route path="/" component={HomePage} exact />
+          <Route path="/about" component={AboutPage} exact />
+          <Route path="/article" component={ArticlePage} exact />
+          <Route path="/articles-list" component={ArticlesList} exact />
+        </div>
+      </div>
+    </Router>
   );
 }
 
