862e1db0410ab2dcd1f22387b3ccadedd698899f
Oleksandra <karska.dev@gmail.com>
2020-08-26 21:18:42 -0700

    Finish back end

--- blog-backend/src/server.js
+++ blog-backend/src/server.js
@@ -4,15 +4,19 @@ import bodyParser from 'body-parser';
 const articlesInfo = {
   'alpha': {
     upvotes: 0,
+    comments: [],
   },
   'betta': {
     upvotes: 0,
+    comments: [],
   },
   'gamma': {
     upvotes: 0,
+    comments: [],
   },
   'delta': {
     upvotes: 0,
+    comments: [],
   }
 }
 
@@ -24,6 +28,15 @@ app.post('/api/articles/:name/upvote', (req, res) => {
   const articleName = req.params.name;
   articlesInfo[articleName].upvotes++;
   res.status(200).send(`${articleName} now has ${articlesInfo[articleName].upvotes} upvotes!`);
-})
+});
+
+app.post('/api/articles/:name/add-comment', (req, res) => {
+  const { username, text } = req.body;
+  const articleName = req.params.name;
+
+  articlesInfo[articleName].comments.push({ username, text });
+
+  res.status(200).send(articlesInfo[articleName]);
+});
 
 app.listen(8000, ()=> console.log('Listening on port 8000'));
