6386e67699c5463184d829cafa64ffad4ee0824c
Oleksandra <karska.dev@gmail.com>
2020-08-26 17:22:19 -0700

    Server part setup

--- blog-backend/src/server.js
+++ blog-backend/src/server.js
@@ -1,13 +1,29 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
+const articlesInfo = {
+  'alpha': {
+    upvotes: 0,
+  },
+  'betta': {
+    upvotes: 0,
+  },
+  'gamma': {
+    upvotes: 0,
+  },
+  'delta': {
+    upvotes: 0,
+  }
+}
+
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/hello', (req, res) => res.send('Hello!'));
-app.get('/hello/:name', (req, res) => res.send(`Hello ${req.params.name}!`));
-
-app.post('/hello', (req, res) => res.send(`Hello ${req.body.name}!`));
+app.post('/api/articles/:name/upvote', (req, res) => {
+  const articleName = req.params.name;
+  articlesInfo[articleName].upvotes++;
+  res.status(200).send(`${articleName} now has ${articlesInfo[articleName].upvotes} upvotes!`);
+})
 
 app.listen(8000, ()=> console.log('Listening on port 8000'));
