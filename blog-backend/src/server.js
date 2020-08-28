1b5b88844a7df7aaec4544b7c1beb716425e08b6
Oleksandra <karska.dev@gmail.com>
2020-08-27 17:31:47 -0700

    First mongoDB request

--- blog-backend/src/server.js
+++ blog-backend/src/server.js
@@ -1,13 +1,25 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import { MongoClient } from 'mongodb'
+import { MongoClient } from 'mongodb';
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('api/articles/:name', (req, res) => {
-  const articleName = req.params.name;
+app.get('api/articles/:name', async (req, res) => {
+  try {
+    const articleName = req.params.name;
+
+    const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true });
+    const db = client.db('blog');
+
+    const articlesInfo = await db.collection('articles').findOne({ name: articleName });
+    res.status(200).json(articleInfo);
+
+    client.close();
+  } catch (error) {
+    res.status(500).json({ message: 'Erroe connecting to db', error });
+  }
 })
 
 app.post('/api/articles/:name/upvote', (req, res) => {
@@ -25,4 +37,4 @@ app.post('/api/articles/:name/add-comment', (req, res) => {
   res.status(200).send(articlesInfo[articleName]);
 });
 
-app.listen(8000, ()=> console.log('Listening on port 8000'));
+app.listen(8000, () => console.log('Listening on port 8000'));
