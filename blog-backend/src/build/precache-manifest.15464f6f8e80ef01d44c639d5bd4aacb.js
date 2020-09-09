65a9231f4d2b6e813881d1da364816ec3dc8d005
Oleksandra <karska.dev@gmail.com>
2020-09-08 19:51:19 -0700

    Ready to publish

---
+++ blog-backend/src/build/precache-manifest.15464f6f8e80ef01d44c639d5bd4aacb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38a06fa24390474444caab9070fef63c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3193a42234dac252cd86",
+    "url": "/static/css/main.4a2cd192.chunk.css"
+  },
+  {
+    "revision": "6ef5d4490cfa4a2711e2",
+    "url": "/static/js/2.fb9d3dad.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.fb9d3dad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3193a42234dac252cd86",
+    "url": "/static/js/main.7851ac2e.chunk.js"
+  },
+  {
+    "revision": "35c6bc6620fd0e11f636",
+    "url": "/static/js/runtime-main.0ed6e50a.js"
+  }
+]);
